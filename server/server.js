1c83ca8b2628597c55ec48152f61fd2be3457a57
AjitLalRana <ar3170149@gmail.com>
2026-06-30 18:57:21 +0530

    vercel-file

--- server/server.js
+++ server/server.js
@@ -13,7 +13,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: 'https://rojsewa-six.vercel.app' || 'http://localhost:5173',
+    origin: ['https://rojsewa-six.vercel.app', 'http://localhost:5173'],
     methods: ['GET', 'POST'],
     credentials: true,
   },
