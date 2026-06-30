f0b1e6e757b9dae8266ac0befe9cccabd3e1e0a0
AjitLalRana <ar3170149@gmail.com>
2026-06-30 18:42:00 +0530

    client-url-setup

--- server/server.js
+++ server/server.js
@@ -13,7 +13,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: process.env.CLIENT_URL || 'http://localhost:5173',
+    origin: 'https://rojsewa-six.vercel.app' || 'http://localhost:5173',
     methods: ['GET', 'POST'],
     credentials: true,
   },
