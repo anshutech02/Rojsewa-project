ac6327b5ad819020c7b81a1180d6d4fdca655d09
AjitLalRana <ar3170149@gmail.com>
2026-07-07 00:04:44 +0530

    nodemailer-smtp

--- server/src/utils/sendEmail.js
+++ server/src/utils/sendEmail.js
@@ -10,11 +10,16 @@ if (!process.env.EMAIL_USER || !process.env.EMAIL_PASS) {
 }
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
+  connectionTimeout: 30000,
+  greetingTimeout: 30000,
+  socketTimeout: 30000,
 });
 
 export const sendEmail = async ({ to, subject, html }) => {
@@ -45,4 +50,4 @@ export const sendEmail = async ({ to, subject, html }) => {
     });
     throw error;
   }
-};
+};
